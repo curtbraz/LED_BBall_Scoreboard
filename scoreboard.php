99d75670fe4c6b2fb9d85fee041c707c51e52523
Curtis Brazzell <curtbraz@gmail.com>
2018-09-07 01:44:22 -0400

    Update scoreboard.php
    Added sound effects and an explosion graphic for points

--- scoreboard.php
+++ scoreboard.php
@@ -4,10 +4,12 @@
 // Accept Scoreboard Input
 if(isset($_REQUEST['red'])){$red = $_REQUEST['red'];}
 if(isset($_REQUEST['blue'])){$blue = $_REQUEST['blue'];}
+if(isset($_REQUEST['redname'])){$redname = $_REQUEST['redname'];} else {$redname = "Red";}
+if(isset($_REQUEST['bluename'])){$bluename = $_REQUEST['bluename'];} else {$bluename = "Blue";}
 
 $padding = "";
 
-$signmessage = "Red ".$red."Blue ".$blue;
+$signmessage = $redname." ".$red.$blue." ".$bluename;
 
 $length = strlen($signmessage);
 
@@ -15,8 +17,8 @@ $padding = 16 - $length;
 
 if($padding == 1){$padding = " ";}
 if($padding == 2){$padding = "  ";}
-if($padding == 3){$padding = "   ";}
-if($padding == 4){$padding = "    ";}
+if($padding == 3){$padding = "  ";}
+if($padding == 4){$padding = "   ";}
 if($padding == 5){$padding = "     ";}
 if($padding == 6){$padding = "      ";}
 if($padding == 7){$padding = "       ";}
@@ -30,7 +32,16 @@ if($padding == 14){$padding = "              ";}
 if($padding == 15){$padding = "               ";}
 if($padding == 16){$padding = "                ";}
 
-$signmessage = "Red ".$red.$padding."Blue ".$blue;
+$signmessage = $redname." ".$red.$padding.$bluename." ".$blue;
+
+if($red != "0" || $blue != "0"){
+
+$boom = 'sudo ./boom.pl';
+exec($boom);
+
+sleep(2);
+
+}
 
 $cmd = 'sudo ./led.pl "'.$signmessage.'"';
 exec($cmd,$output);
